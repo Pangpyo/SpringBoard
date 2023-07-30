603258f7215b5c9de00eac001318dcfcf5ec1498
김광표 <kkp0639@gmail.com>
2023-07-30 16:20:59 +0900

    docs : 스웨거 주석 및 스웨거 api 문서 추가

--- board/src/main/java/com/nts/board/api/BoardController.java
+++ board/src/main/java/com/nts/board/api/BoardController.java
@@ -3,6 +3,8 @@ package com.nts.board.api;
 import com.nts.board.BoardCountDto;
 import com.nts.board.comment.application.CommentService;
 import com.nts.board.post.application.PostService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Api( tags = "메인화면 공통 API" )
 public class BoardController {
     private final PostService postService;
     private final CommentService commentService;
 
     @GetMapping("/count")
+    @ApiOperation(value = "전체 게시글, 댓글 수 조회")
     public ResponseEntity<BoardCountDto> boardCount() {
         Long postCount = postService.countPost();
         Long commentCount = commentService.countComment();
